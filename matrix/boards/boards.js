ee3f89078afd736e71b13f9323fdc0c6cf9c3e49
Hermitter <chacin.carlos9798@gmail.com>
2019-12-18 16:45:56 -0500

    updated matrix lite module

--- matrix/boards/boards.js
+++ matrix/boards/boards.js
@@ -7,4 +7,4 @@ const info = require("@matrix-io/matrix-lite").info;
 const creator = require("./creator");
 const voice = require("./voice");
 
-module.exports = info.deviceType() === "MATRIX Creator" ? creator : voice;
+module.exports = info.deviceType === "MATRIX Creator" ? creator : voice;
